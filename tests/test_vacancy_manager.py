7f1119a243d4207d0f8a67722d924e44a2109b7a
Lada Anpilogova <lada2fox@gmail.com>
2025-01-10 00:01:50 +0300

    tests

---
+++ tests/test_vacancy_manager.py
@@ -0,0 +1,15 @@
+import pytest
+from src.vacancy import Vacancy, VacancyManager
+
+
+def test_compare_vacancies_by_salary():
+    vacancy1 = Vacancy("Python Developer", "url1", 100000, "Description 1", "Company 1")
+    vacancy2 = Vacancy("Java Developer", "url2", 120000, "Description 2", "Company 2")
+    sorted_vacancies = VacancyManager.compare_vacancies_by_salary([vacancy1, vacancy2])
+    assert sorted_vacancies[0].salary == 120000
+
+
+def test_vacancy_validation():
+    vacancy = Vacancy("Python Developer", "url1", None, "Description 1", "Company 1")
+    VacancyManager.validate_vacancy_data(vacancy)
+    assert vacancy.salary == 0
