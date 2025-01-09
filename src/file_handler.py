77a33560e5a339463fd0606828398394c7f6496e
Lada Anpilogova <lada2fox@gmail.com>
2025-01-09 23:19:44 +0300

    добавлен модуль для работы с файлами и добвлен класс FileHandler для сохранения вакансий в JSON файл

---
+++ src/file_handler.py
@@ -0,0 +1,23 @@
+import json
+import os
+import logging
+from typing import List
+from src.vacancy import Vacancy
+
+logger = logging.getLogger(__name__)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+path = os.path.join(BASE_DIR, "logs", "file_handler.log")
+file_handler = logging.FileHandler(path, encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
+
+
+class FileHandler:
+    @staticmethod
+    def save_to_json(file_path: str, vacancies: List[Vacancy]):
+        """Сохранение вакансий в JSON файл"""
+        with open(file_path, "w", encoding="utf-8") as f:
+            json.dump([v.__dict__ for v in vacancies], f, ensure_ascii=False, indent=4)
+        logger.info(f"Вакансии сохранены в файл: {file_path}")
