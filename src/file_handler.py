f240cfe676c2a328e4b4d6c1c9b52bc2a04b1da6
Lada Anpilogova <lada2fox@gmail.com>
2025-01-09 23:56:03 +0300

    добавлен метод для загрузки вакансий из JSON файла

--- src/file_handler.py
+++ src/file_handler.py
@@ -21,3 +21,18 @@ class FileHandler:
         with open(file_path, "w", encoding="utf-8") as f:
             json.dump([v.__dict__ for v in vacancies], f, ensure_ascii=False, indent=4)
         logger.info(f"Вакансии сохранены в файл: {file_path}")
+
+    @staticmethod
+    def load_from_json(file_path: str) -> List[Vacancy]:
+        """Загрузка вакансий из JSON файла"""
+        try:
+            with open(file_path, "r", encoding="utf-8") as f:
+                data = json.load(f)
+                logger.info(f"Вакансии загружены из файла: {file_path}")
+                return [Vacancy(**vacancy) for vacancy in data]
+        except FileNotFoundError:
+            logger.warning(f"Файл не найден: {file_path}")
+            return []
+        except Exception as e:
+            logger.error(f"Ошибка при загрузке файла: {e}")
+            return []
